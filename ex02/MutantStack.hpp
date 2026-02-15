e0f54805b823bc8a23f1ef011346c61c17c190c3
Chiara <chiaryarra@gmail.com>
2026-02-15 16:21:05 -0300

    Feat: Ex02 completely done

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <stack>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+private:
+public:
+    MutantStack() : std::stack<T>() {};
+    MutantStack(const MutantStack& copy) : std::stack<T>(copy) {}
+    MutantStack& operator=(const MutantStack& copy) 
+    {
+        if (this != &copy)
+            std::stack<T>::operator=(copy);
+        return (*this);
+    }
+    ~MutantStack() {};
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+
+    iterator begin() {
+        return (this->c.begin());
+    }
+    iterator end() {
+        return (this->c.end());
+    }
+};
+
